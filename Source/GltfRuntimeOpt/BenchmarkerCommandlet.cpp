32ea42e773f83e0e05a20474c5939db68b053fb8
Sietze Riemersma <43845930+KungFuDonkey@users.noreply.github.com>
2023-06-14 11:03:39 +0200

    added benchmarker

---
+++ Source/GltfRuntimeOpt/BenchmarkerCommandlet.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BenchmarkerCommandlet.h"
+
+#include "glTFRuntimeParser.h"
+
+DEFINE_LOG_CATEGORY(GLTFBenchmarker)
+
+#define LOG(x,...) UE_LOG(GLTFBenchmarker, Log, TEXT(x), __VA_ARGS__)
+
+int32 UBenchmarkerCommandlet::Main(const FString& Params)
+{
+	LOG("Hello");
+	FGenericPlatformProcess::ConditionalSleep([this](){return true;}, 5.0f);
+	FglTFRuntimeParser parser;
+	return 0;
+}
---
+++ Source/GltfRuntimeOpt/BenchmarkerCommandlet.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Commandlets/Commandlet.h"
+#include "UObject/Object.h"
+#include "BenchmarkerCommandlet.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(GLTFBenchmarker, Log, All)
+
+/**
+ * 
+ */
+UCLASS()
+class GLTFRUNTIMEOPT_API UBenchmarkerCommandlet : public UCommandlet
+{
+	GENERATED_BODY()
+public:
+	virtual int32 Main(const FString& Params) override;
+};
